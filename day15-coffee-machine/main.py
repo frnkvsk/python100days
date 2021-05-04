6b80d8b9b1e7104f451630ea0dfa669a2c8767df
frnkvsk <frnk05vas@zoho.com>
2021-05-04 14:40:14 -0700

    finished day 15

--- day15-coffee-machine/main.py
+++ day15-coffee-machine/main.py
@@ -1,58 +1,83 @@
 
-class Latte:
-   def __init__(self, beans, water, milk, syrup):
-      self.beans = beans
-      self.water = water
-      self.milk = milk
-      self.syrup = syrup
-      self.
-class Cappuccino:
-   def __init__(self, beans, water, milk):
-      self.beans = beans
-      self.water = water
-      self.milk = milk
-class PumpkinSpice:
-   def __init__(self, beans, water, milk, spices, vanilla):
-      self.beans = beans
-      self.water = water
-      self.milk = milk
-      self.spices = spices
-      self.vanilla = vanilla
-   
-latte = Latte(100, 100, 100, 100)
-cappuccino = Cappuccino(100, 100, 100)
-pumpkinSpice = PumpkinSpice(100, 100, 100, 100, 100)
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+profit = 0
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
 
-def totalBeans():
-   return latte.beans + cappuccino.beans + pumpkinSpice.beans
-def totalWater():
-   return latte.water + cappuccino.water + pumpkinSpice.water
-def totalMilk():
-   return latte.milk + cappuccino.milk + pumpkinSpice.milk
-def totalSyrup(): 
-   return latte.syrup
-def totalSpices():
-   return pumpkinSpice.spices
-def totalVanilla():
-   return pumpkinSpice.vanilla
- 
 userInput = ''
 
+def process_coin(amt, moneyIn):
+   """Returns the amount of each coin"""
+   if moneyIn == 'quarter' or moneyIn == 'quarters':
+      return amt * .25
+   elif moneyIn == 'dime' or moneyIn == 'dimes':
+      return amt * .10
+   elif moneyIn == 'nickel' or moneyIn == 'nickels':
+      return amt * .05
+   elif moneyIn == 'penny' or moneyIn == 'pennies':
+      return amt * .01
+
+def is_resources_sufficient(order_in):
+   """Returns true if enough resources are available and false if not"""
+   for x in order_in:
+      if resources[x] < order_in[x]:            
+         print(f'Sorry there is not enough {x}')
+         return False
+   return True
+
 while userInput != 'off':
-   print('What would you like? (espresso/latte/cappuccino):')
-   userInput = input()
+   userInput = input('What would you like? (espresso/latte/cappuccino): ').lower()
    if userInput == 'report':
       print("""
-      Beans: %sg
       Water: %sml
       Milk: %sml
-      Syrup: $%sml
-      Spices: %sg
-      Vanilla: %sml
-      """ % (totalBeans(), totalWater(), totalMilk(), totalSyrup(), totalSpices(), totalVanilla()))
-   elif userInput.upper() == 'LATTE':
-      print('Latte')
-   elif userInput.upper() == 'CAPPUCCINO':
-      print('Cappuccino')
-   elif userInput.upper() == 'PUMPKINSPICE':
-      print('Pumpkin Spice')
+      Coffee: %sg
+      Money: $format(%s, '.2f')
+      """ % (resources['water'], resources['milk'], resources['coffee'], format(profit, '.2f')))
+   elif userInput == 'espresso' or userInput == 'latte' or userInput == 'cappuccino':
+      ingredients = MENU[userInput]['ingredients']
+      cost = float(MENU[userInput]['cost'])
+
+      if is_resources_sufficient(ingredients):
+         money_in = input(f'Your cost is ${format(cost,".2f")} please insert coins ').lower().split(',')
+         for x in money_in:  
+            moneyEach = x.split() 
+            cost -= process_coin(float(moneyEach[0]), moneyEach[1])
+         if cost <= 0:
+            for e in ingredients:
+               resources[e] = resources[e] - ingredients[e]
+            profit = profit + float(MENU[userInput]['cost'])
+            if cost < 0:
+               print(f'Here is ${format(cost * -1, ".2f")} dollars in change.')
+            print(f'Here is you {userInput}. Enjoy!')
+         else:
+            print("Sorry that's not enough money. Money refunded.")
+   else:
+      print(f"Sorry we don't have {userInput}, maybe you spelt it wrong?")
